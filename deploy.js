501961d1a76a797648805b0fb7176ba0050df6f8
John Ferland <johnferlandmi@gmail.com>
2022-01-12 13:04:45 -0500

    new deploy.js ip

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.203.85.30',
+      host: '54.211.252.254',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
